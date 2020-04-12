a251d91bfd32f0e74bcbba07584da995352aca08
laizhili <laizhili1106@163.com>
2020-04-12 20:55:43 +0800

    test3提交

--- edugit.php
+++ edugit.php
@@ -9,6 +9,6 @@ class test
 {
     public function git()
     {
-        echo 'hello';
+        echo 'hello test3';
     }
 }
--- test/kffz3.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: laizhili
- * Date: 2020/4/12
- * Time: 4:49 下午
- */
-echo 'this is kffz3';
